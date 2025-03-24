766edfbf00f2986aa3b8f3e564167291ef68bb3b
B00165631 Alfred Ukpong <B00165631@mytudublin.ie>
2025-03-24 17:32:10 +0000

    Completed Initial GUI interface

--- MyFirstGUI.py
+++ MyFirstGUI.py
@@ -1,33 +1,47 @@
+# Alfred Ukpong
+# 24/03/25
+# Week 9 
+
 from tkinter import *
+from tkinter import messagebox
 
 class MyFirstGUI:
-    
     def __init__(self, master):
         self.master = master
-        master.title("A simple GUI")
-        
-        self.label1 = Label(master, text="Enter your Firstname")
-        self.label1.pack()
-        self.entry1 = Entry()
-        self.entry1.pack()
+        master.title("Tennis Club Member Form")
 
-        self.label2 = Label(master, text="Enter your Surname")
-        self.label2.pack()
-        self.entry2 = Entry()
-        self.entry2.pack()
-        
-        self.helloButton = Button(master, text="Hello", command=self.sayhello)
-        self.helloButton.pack()
+        Label(master, text="Enter your Firstname").pack()
+        self.entry_firstname = Entry(master)
+        self.entry_firstname.pack()
 
-        self.closeButton = Button(master, text="Close", command=self.close)
-        self.closeButton.pack()
+        Label(master, text="Enter your Surname").pack()
+        self.entry_surname = Entry(master)
+        self.entry_surname.pack()
 
-    def sayhello(self):    
-        print("Hello " + self.entry1.get())
+        Label(master, text="Enter your Date of Birth (YYYY-MM-DD)").pack()
+        self.entry_dob = Entry(master)
+        self.entry_dob.pack()
 
-    def close(self):
-        root.destroy()
+        Label(master, text="Enter your Member Type (Junior/Intermediate/Senior)").pack()
+        self.entry_membertype = Entry(master)
+        self.entry_membertype.pack()
+
+        self.insert_button = Button(master, text="Insert Into DB", command=self.insert_member)
+        self.insert_button.pack(pady=5)
+
+        self.print_button = Button(master, text="Print All Members", command=self.print_members)
+        self.print_button.pack(pady=5)
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack(pady=5)
+
+    def insert_member(self):
+        messagebox.showinfo("Insert", "Successful.")
+
+    def print_members(self):
+        messagebox.showinfo("Print", "Successful")
 
 root = Tk()
 my_gui = MyFirstGUI(root)
-root.dooneevent()
+root.mainloop()
+
